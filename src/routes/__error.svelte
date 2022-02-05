5bf032cf9fbd23a0409919ad388734435c86f2b8
bobrovice <bobrovice@gmail.com>
2022-02-05 20:43:38 +0300

    add pages, upd ui

---
+++ src/routes/__error.svelte
@@ -0,0 +1,14 @@
+<section>
+    <h1>404</h1>
+	<h3>Page not found</h3>
+	<a href="/">Return to home page</a>
+</section>
+
+<style>
+section {
+    display: grid;
+    grid-gap: 1rem;
+    align-self: center;
+    justify-self: center;
+}
+</style>
